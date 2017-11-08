2300f2de08d0f841f185b99db9ae11b0aae71fc7
SebastianStokkebro <32563781+SebastianStokkebro@users.noreply.github.com>
2017-11-08 16:14:17 +0100

    Update GoldGrindr.java
    ændret konto klasse bruges nu ved nedarv af spiller

--- 34_del2/src/GoldGrindr.java
+++ 34_del2/src/GoldGrindr.java
@@ -6,160 +6,138 @@ import desktop_resources.GUI;
 
 /**
  * 
- * @author Alan
  * @author Andreas
  * @author Asef
- * @author Burhan
  * @author David
+ * @author Alan
  * @author Peter
+ * @author Burhan
  * @author Sebastian
  *
  */
 
 public class GoldGrindr {
 
-	public static void main (String[] args) {
-		
+	public static void main(String[] args) {
+
 		Scanner keyb = new Scanner(System.in);
-		
+		/**
+		 * annonym objektoprettelse af klassen Felt.
+		 */
 		new Felt();
-		
+
 		/**
 		 * Spillerne oprettes med navn og konto samt tilhørende biler.
 		 */
 		System.out.println("Indtast navn på spiller 1");
-		Spiller spiller1 = new Spiller(keyb.next());
-		Konto konto1 = new Konto(1000);
-		Car c1 = new Car.Builder()
-				.typeRacecar()
-				.patternHorizontalDualColor()
-				.primaryColor(Color.red)
-				.secondaryColor(Color.blue)
-				.build();
-		
+		Spiller spiller1 = new Spiller(1000, keyb.next());
+		Car c1 = new Car.Builder().typeRacecar().patternHorizontalDualColor().primaryColor(Color.red)
+				.secondaryColor(Color.blue).build();
+
 		System.out.println("Indtast navn på spiller 2");
-		Spiller spiller2 = new Spiller(keyb.next());
-		Konto konto2 = new Konto(1000);
-		Car c2 = new Car.Builder()
-				.typeRacecar()
-				.patternHorizontalDualColor()
-				.primaryColor(Color.black)
-				.secondaryColor(Color.yellow)
-				.build();
-		
+		Spiller spiller2 = new Spiller(1000, keyb.next());
+		Car c2 = new Car.Builder().typeRacecar().patternHorizontalDualColor().primaryColor(Color.black)
+				.secondaryColor(Color.yellow).build();
+
 		keyb.close();
-		
-		GUI.addPlayer(spiller1.getNavn(), konto1.getPenge(), c1);
-		GUI.addPlayer(spiller2.getNavn(), konto2.getPenge(), c2);
+
+		GUI.addPlayer(spiller1.getNavn(), spiller1.getPenge(), c1);
+		GUI.addPlayer(spiller2.getNavn(), spiller2.getPenge(), c2);
 		GUI.setCar(1, spiller1.getNavn());
 		GUI.setCar(1, spiller2.getNavn());
-		
+
 		boolean done = false;
 		boolean s1tur = true;
-		boolean k1 = true;
 		Spiller nuvSpiller = spiller1;
-		Konto nuvKonto = konto1;
-		
+
 		while (!done) {
-			
+
 			/**
-			 * Ternary betingelse der afgør hvis tur det er,
-			 * samt hvis konto der skal bruges.
+			 * Ternary betingelse der afgør hvis tur det er.
 			 */
-			nuvSpiller = s1tur ? spiller1: spiller2;
-			nuvKonto = k1 ? konto1: konto2;
+			nuvSpiller = s1tur ? spiller1 : spiller2;
 			
 			System.out.println("Det er din tur " + nuvSpiller.getNavn() + " kast terning på OK");
-			
+
 			Terning t1 = new Terning();
-			Terning t2 = new Terning ();
+			Terning t2 = new Terning();
 			GUI.setDice(t1.kast(), t2.kast());
-			
-			int feltnummer = t1.getVærdi()+t2.getVærdi();
+
+			int feltnummer = t1.getVærdi() + t2.getVærdi();
 			GUI.removeCar(1, nuvSpiller.getNavn());
 			GUI.setCar(feltnummer, nuvSpiller.getNavn());
-			
+
 			/**
-			 * switch-case der afgør hvad der skal ske med spilleren
-			 * på det givne felt.
+			 * switch-case der afgør hvad der skal ske med spilleren på det givne felt.
 			 */
-			switch(feltnummer) {
+			switch (feltnummer) {
 			case 2:
 				GUI.showMessage(Læs.laes("towerBesked:"));
-				nuvKonto.tilføjPenge(250);
+				nuvSpiller.tilføjPenge(250);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 3:
 				GUI.showMessage(Læs.laes("craterBesked:"));
-				nuvKonto.fratrækPenge(100);
+				nuvSpiller.fratrækPenge(100);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 4:
 				GUI.showMessage(Læs.laes("placeGatesBesked:"));
-				nuvKonto.tilføjPenge(100);
+				nuvSpiller.tilføjPenge(100);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 5:
 				GUI.showMessage(Læs.laes("coldDesertBesked:"));
-				nuvKonto.fratrækPenge(20);
+				nuvSpiller.fratrækPenge(20);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 6:
 				GUI.showMessage(Læs.laes("walledCityBesked:"));
-				nuvKonto.tilføjPenge(180);
+				nuvSpiller.tilføjPenge(180);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 7:
 				GUI.showMessage(Læs.laes("monasteryBesked:"));
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 8:
 				GUI.showMessage(Læs.laes("blackCaveBesked:"));
-				nuvKonto.fratrækPenge(70);
+				nuvSpiller.fratrækPenge(70);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 9:
 				GUI.showMessage(Læs.laes("hutsBesked:"));
-				nuvKonto.tilføjPenge(60);
+				nuvSpiller.tilføjPenge(60);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 10:
 				GUI.showMessage(Læs.laes("wereBesked:"));
-				nuvKonto.fratrækPenge(80);
+				nuvSpiller.fratrækPenge(80);
 				break;
 			case 11:
 				GUI.showMessage(Læs.laes("pitBesked:"));
-				nuvKonto.fratrækPenge(50);
+				nuvSpiller.fratrækPenge(50);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			case 12:
 				GUI.showMessage(Læs.laes("goldmineBesked:"));
-				nuvKonto.tilføjPenge(650);
+				nuvSpiller.tilføjPenge(650);
 				s1tur = !s1tur;
-				k1 = !k1;
 				break;
 			}
-			GUI.setBalance(nuvSpiller.getNavn(), nuvKonto.getPenge());
+			GUI.setBalance(nuvSpiller.getNavn(), nuvSpiller.getPenge());
 			GUI.removeCar(feltnummer, nuvSpiller.getNavn());
 			GUI.setCar(1, nuvSpiller.getNavn());
-			
+
 			/**
 			 * if-betingelse der bryder løkken når spilleren opnår 3000 penge eller derover.
 			 */
-			if(nuvKonto.getPenge() >= 3000) {
+			if (nuvSpiller.getPenge() >= 3000) {
 				done = true;
 				GUI.showMessage(Læs.laes("vundetBesked:") + " " + nuvSpiller.getNavn().toUpperCase() + "!!!!!");
 			}
 		}
 	}
-	
+
 }
+
